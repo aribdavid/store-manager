aa805913d0069ecb7fa8a3e6506f5a1249b67085
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-09 18:07:22 +0000

    feat: getById model from product model

--- models/productModel.js
+++ models/productModel.js
@@ -7,6 +7,14 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [products] = await connection.execute(query, [id]);
+
+  return products;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
