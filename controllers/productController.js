1a266fd23ddaf809c884a897e30c164d2fb9cf70
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-09 18:13:01 +0000

    feat: refactored getAll from productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,9 @@
 const productService = require('../services/productService');
 
 const getAll = async (_request, response, _next) => {
-  try {
-    const products = await productService.getAll();
-
-    return response.status(200).json(products);
-  } catch (error) {
-    return response.status(error.status).json({ message: error.message });
-  }
+    await productService.getAll()
+    .then((res) => response.status(200).json(res))
+    .catch((error) => response.status(error.status).json({ message: error.message }));
 };
 
 module.exports = {
