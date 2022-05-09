57e4743b77b4e5e03b13d377115ceee61acd16e5
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-09 18:22:02 +0000

    feat: created getById, createProduct, updateProduct, deleteProduct from productService

--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,5 @@
 const productModel = require('../models/productModel');
- const errorHandler = require('../utils/errorHandler');
+const errorHandler = require('../utils/errorHandler');
 
 const getAll = async () => {
    const products = await productModel.getAll();
@@ -8,6 +8,36 @@ const getAll = async () => {
   return products;
 };  
 
+const getById = async (id) => {
+  const products = await productModel.getById(id);
+  if (products.length === 0) throw errorHandler(404, 'Product not found');
+
+  return products[0];
+};
+
+const createProduct = async (name, quantity) => {
+  const productExists = await productModel.getByName(name);
+  if (productExists.length > 0) throw errorHandler(409, 'Product already exists');
+  const product = await productModel.createProduct(name, quantity);
+  return product;
+};
+
+const updateProduct = async (id, name, quantity) => {
+  await getById(id);
+  const product = await productModel.updateProduct(id, name, quantity);
+
+  return product;
+};
+
+const deleteProduct = async (id) => {
+  await getById(id);
+  await productModel.deleteProduct(id);
+};
+
 module.exports = {
   getAll,
+  getById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
