5125f3250577a975c944a1e191f031a882080549
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-09 18:30:39 +0000

    feat: created getById, createProduct, updateProduct, deleteProduct from productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,6 +6,39 @@ const getAll = async (_request, response, _next) => {
     .catch((error) => response.status(error.status).json({ message: error.message }));
 };
 
+const getById = async (request, response, _next) => {
+    const { id } = request.params;
+     await productService.findById(id)
+     .then((res) => response.status(200).json(res))
+     .catch((error) => response.status(error.status).json({ message: error.message }));
+};
+
+const createProduct = async (request, response) => {
+    const { name, quantity } = request.body;
+    await productService.createProduct(name, quantity)
+    .then((res) => response.status(201).json(res))
+    .catch((error) => response.status(error.status).json({ message: error.message }));
+};
+
+const updateProduct = async (request, response, _next) => {
+    const { id } = request.params;
+    const { name, quantity } = request.body;
+     await productService.updateProduct(id, name, quantity)
+     .then((res) => response.status(200).json(res))
+     .catch((error) => response.status(error.status).json({ message: error.message }));
+};
+
+const deleteProduct = async (request, response, _next) => {
+    const { id } = request.params;
+    await productService.deleteProduct(id)
+    .then((_res) => response.status(204).send())
+    .catch((error) => response.status(error.status).json({ message: error.message }));
+};
+
 module.exports = {
   getAll,
+  getById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
